e36a326af2a4b01e0e5f8174045b83d5a7809b6a
yrribeiro <ynk9a@outlook.com>
2021-09-13 18:17:56 -0300

    folder reorganized

--- djikstra.c
+++ djikstra.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <limits.h>
 
-
 void djikstra(int vertex, int graph[][vertex]);
 void readData();
 void createEdge(int vertex, int graph[vertex][vertex], int src, int dest, int weight);
@@ -17,6 +16,7 @@ int main(){
 
 void djikstra(int vertex, int graph[][vertex]){
     int distancia[vertex], precedente[vertex], visitas[vertex], x = 0, z = 0, dist_min = INT_MAX;
+    int vertice_inicial = 0, vertice_final = 6, soma = 0;
     distancia[0] = 0;
     precedente[0] = -1;
     visitas[0] = 0;
@@ -42,10 +42,25 @@ void djikstra(int vertex, int graph[][vertex]){
             }
             z++;
         }
-        
+
     }
+    int  i = 0, y;
+    while(i < vertice_final)
+    {
+        y = i;
+        while (vertice_inicial != y)
+        {
+            soma += distancia[y];
+            y = precedente[y];
+
+        }
+        printf("%d:%d ", i+1,  soma);
+        soma = 0;
+        i++;
+    }
+    printf("\n");
     for (int y = 0; y < vertex; y++){
-        printf("para chegar em %d, distancia: %d , precedente: %d\n", y+1,  distancia[y], precedente[y]+1);
+        printf("para chegar em %d, distancia :%d, precendente %d\n", y+1,  distancia[y], precedente[y]+1);
     }
 }
 
@@ -60,7 +75,8 @@ void initializeMatrix(int vertex, int graph[vertex][vertex]){
 void readData(){
     FILE *data;
     int vertex=0, edges=0, src=0, dest=0, weight=0, i=0;
-    data = fopen("diska_input.input", "r");
+    data = fopen("sample.input", "r");
+
     if (data==NULL){
         fputs("Cannot open source file\n", stderr);
         exit(EXIT_FAILURE);
@@ -68,9 +84,10 @@ void readData(){
         size_t maxLen = 6;
         char *line= malloc(6*sizeof(char));
         vertex = fgetc(data) - '0';
-        printf("%d\n", vertex);
+
         int graph[vertex][vertex];
         initializeMatrix(vertex, graph);
+
         while (getline(&line, &maxLen, data) > 0)
         {
             if (i != 0)
@@ -84,18 +101,20 @@ void readData(){
                 else
                     weight = 1;
                 createEdge(vertex, graph, src-1, dest-1, weight);
- 
-            }
 
+            }
             i++;
         }
-        printf("\n");
-        for(int x = 0; x < vertex; x++){
-            for(int k=0; k< vertex; k++)
-                printf("%d ", graph[x][k]);
-            printf("\n");
-        }
+
+        // para printar a matriz
+        // for(int x = 0; x < vertex; x++){
+        //     for(int k=0; k< vertex; k++)
+        //         printf("%d ", graph[x][k]);
+        //     printf("\n");
+        // }
+
         djikstra(vertex, graph);
+
         if (line)
             free(line);
         fclose(data);
