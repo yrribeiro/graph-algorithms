658dd3fdc24d885c83360e046aa1676bdb6a7fc0
yrribeiro <ynk9a@outlook.com>
2021-09-18 11:14:20 -0300

    bellman refactored + limits.h bug fixed

--- bellman-ford/bellman-ford.c
+++ bellman-ford/bellman-ford.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#define MAX_VERTEX 20
 
 typedef struct EDGE{
     int src, dest, weight;
 }EDGE;
 
-typedef struct EDGE_LIST{
-    EDGE data[20];
-}EDGE_LIST;
-
-void bellmanFord(int totalVertex, int distance[], int root[], int numOfArchs, EDGE_LIST arch, int src);
+void bellmanFord(int totalVertex, int distance[], int root[], int numOfArchs, EDGE *arch, int src);
 void printPath (int src, int dest, int root[]);
 
 int main(){
     int totalVertex, i=0, j, src;
-    EDGE_LIST arch;
+    EDGE arch[MAX_VERTEX];
     int numOfArchs=0;
 
     scanf("%d %d", &totalVertex, &src);
     int distance[totalVertex], root[totalVertex], graph[totalVertex][totalVertex];
 
     for (i=0; i<totalVertex; i++){
-        distance[i] = INT_MAX;
+        distance[i] = SHRT_MAX;
         root[i] = -1;
         for (j=0; j<totalVertex; j++){
             scanf("%d", &graph[i][j]);
             if (graph[i][j]){
-                arch.data[numOfArchs].src = i;
-                arch.data[numOfArchs].dest = j;
-                arch.data[numOfArchs].weight = graph[i][j];
+                arch[numOfArchs].src = i;
+                arch[numOfArchs].dest = j;
+                arch[numOfArchs].weight = graph[i][j];
                 numOfArchs++;
             }
         }
@@ -41,13 +38,13 @@ int main(){
     return 0;
 }
 
-void bellmanFord(int totalVertex, int distance[], int root[], int numOfArchs, EDGE_LIST arch, int src){
-    int i=1, j, start, end;
+void bellmanFord(int totalVertex, int distance[], int root[], int numOfArchs, EDGE *arch, int src){
+    int i=1, j, end;
     while(i<totalVertex){
         for (j=0; j<numOfArchs; j++){
-            if (distance[arch.data[j].dest] > distance[arch.data[j].src] + arch.data[j].weight){
-                distance[arch.data[j].dest] = distance[arch.data[j].src] + arch.data[j].weight;
-                root[arch.data[j].dest] = arch.data[j].src;
+            if (distance[arch[j].dest] > distance[arch[j].src] + arch[j].weight){
+                distance[arch[j].dest] = distance[arch[j].src] + arch[j].weight;
+                root[arch[j].dest] = arch[j].src;
             }
         }
         i++;
